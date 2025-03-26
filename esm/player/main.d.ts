0ed2ec9179cb760243c5b70e9e1343c62907aa51
jameshi16 <1856293+jameshi16@users.noreply.github.com>
2025-03-26 16:10:19 +0000

    include player

---
+++ esm/player/main.d.ts
@@ -0,0 +1,3 @@
+import { Dictionary, DictionaryArray, EnvelopeType, InstrumentType, Transition, Chord, Envelope, Config } from "../synth/SynthConfig";
+import { NotePin, Note, Pattern, Instrument, Channel, Synth } from "../synth/synth";
+export { Dictionary, DictionaryArray, EnvelopeType, InstrumentType, Transition, Chord, Envelope, Config, NotePin, Note, Pattern, Instrument, Channel, Synth };
